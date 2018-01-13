c2c688494bbe85098a5863285a98419b143d6116
asc686f61 <asc689561@yahoo.com>
2018-01-13 23:50:12 +0700

    The script install chrome

--- install/chrome.sh
+++ install/chrome.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
-sudo dpkg -i ./google-chrome*.deb
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i ./google-chrome*.deb
 sudo apt-get install -f
