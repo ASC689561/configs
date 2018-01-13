3fda9d335be0979d89ce4e22bfc6936637ac93d1
asc686f61 <asc689561@yahoo.com>
2018-01-13 23:49:13 +0700

    The script install chrome

--- install/chrome.sh
+++ install/chrome.sh
@@ -1,4 +1,4 @@
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update
-sudo apt-get install google-chrome-stable
+#!/usr/bin/env bash
+sudo dpkg -i ./google-chrome*.deb
+sudo dpkg -i ./google-chrome*.deb
+sudo apt-get install -f
