27dae8d53fd191b088febfe37e39a9abb7587a18
Nguyễn Bá Hòa <asc686f61@gmail.com>
2019-11-13 11:55:44 +0700

    del

--- install/consul-config.sh
+++
@@ -1,9 +0,0 @@
-consul kv put consul-alerts/config/checks/change-threshold 30
-consul kv put consul-alerts/config/notifiers/slack/enabled true
-consul kv put consul-alerts/config/notifiers/slack/username k8s.misa.com.vn
-consul kv put consul-alerts/config/notifiers/slack/channel rd-alerts
-consul kv put consul-alerts/config/notifiers/slack/detailed true
-consul kv put consul-alerts/config/notifiers/slack/url https://hooks.slack.com/services/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
-
-
-
